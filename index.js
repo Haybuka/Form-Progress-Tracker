f5f103f5c853a5c7ca6049615384f0d7e47489d9
haybuka <paschal.nduleo@gmail.com>
2023-05-11 14:20:39 +0100

    Added label movement with js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Get relevant Tags
+
 let burger = document.querySelector(".burger");
 const biodataSection = document.querySelector(".biodata");
 const allListItems = document.querySelectorAll("li");
@@ -6,30 +8,49 @@ const stepFrom = document.querySelector(".step-from");
 const stepTo = document.querySelector(".step-to");
 const form = document.querySelector("form");
 
-let initialStep = 0;
-stepFrom.innerHTML = initialStep + 1;
-stepTo.innerHTML = allListItems.length;
+//Toggling Burger collapse
 burger.addEventListener("click", (event) => {
   biodataSection.classList.toggle("inactive");
 });
 
+//initial step for first element
+let initialStep = 0;
+
+//Adding count to step element, +1 to kick off 0.
+stepFrom.innerHTML = initialStep + 1;
+stepTo.innerHTML = allListItems.length;
+
+//Form submit handles movement
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  //Get the length of the List items to determine steps
   const allSteps = allListItems.length;
 
+  // Handle step through and increments.
   if (initialStep + 1 !== allSteps) {
-    console.log(initialStep + 1, allSteps);
     initialStep += 1;
+
+    //Select all labels in form to activate thier view
     allLabels[initialStep].classList.add("active-input");
     allLabels[initialStep - 1].classList.remove("active-input");
+
+    //Select list items to jump through
     allListItems[initialStep].classList.add("active");
     allListItems[initialStep - 1].classList.remove("active");
+
+    //Increment step element in span
+
     stepFrom.innerHTML = initialStep + 1;
-    console.log("added one to step", initialStep);
   } else {
+    //On form complete, change button text.
     document.querySelector("button").innerHTML = "Submit";
-    console.log("all steps done", initialStep);
+
+    // can also pull out all form field values.
+    const allInputs = form.querySelectorAll('input');
+    allInputs.forEach( Input => {
+        console.log(`${Input.id}`, Input.value)
+    })
   }
 });
 
-// console.dir(allListItems.length);
+
