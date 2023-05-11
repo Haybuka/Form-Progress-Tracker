ef15e9a9231b59c5a8964765703254d7105b5db7
haybuka <paschal.nduleo@gmail.com>
2023-05-11 13:07:27 +0100

    Added label movement with js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let burger = document.querySelector(".burger");
 const biodataSection = document.querySelector(".biodata");
 const allListItems = document.querySelectorAll("li");
+const allLabels = document.querySelectorAll("label");
 const stepFrom = document.querySelector(".step-from");
 const stepTo = document.querySelector(".step-to");
 const form = document.querySelector("form");
@@ -19,6 +20,8 @@ form.addEventListener("submit", (event) => {
   if (initialStep + 1 !== allSteps) {
     console.log(initialStep + 1, allSteps);
     initialStep += 1;
+    allLabels[initialStep].classList.add("active-input");
+    allLabels[initialStep - 1].classList.remove("active-input");
     allListItems[initialStep].classList.add("active");
     allListItems[initialStep - 1].classList.remove("active");
     stepFrom.innerHTML = initialStep + 1;
